be22494d1c1da9b5ae16059d8f4b60db95ec915a
victorutiu <victor.utiu@stud.ubbcluj.ro>
2026-05-31 17:16:42 +0300

    fix websocket production

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -3,7 +3,11 @@ import nodemailer from "nodemailer"
 const transporter =
   nodemailer.createTransport({
 
-    service: "gmail",
+    host: "smtp.gmail.com",
+
+    port: 587,
+
+    secure: false,
 
     auth: {
       user:
